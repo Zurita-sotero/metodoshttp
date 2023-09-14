c75641e222a0d3dc40570c8ecbef393e2178391d
Sotero de Jesus Zurita Mota <isc2030328@gmail.com>
2023-09-14 14:02:06 -0600

    Agregando cambios a metodoshttp

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require ('express')
+
+const app = express()
+
+app.get('/', (request, response) =>{
+    response.send("Hola GET")
+})
+
+app.get('/', (request, response) =>{
+    response.send("Hola POST")
+})
+app.get('/', (request, response) =>{
+    response.send("Hola PUT")
+})
+app.get('/', (request, response) =>{
+    response.send("Hola PATCH")
+})
+app.get('/', (request, response) =>{
+    response.send("Hola DELETE")
+})
+
+/**
+ * FUNCIONALIDAD
+ * 
+ */
+
+//http://localhost:3000/
+app.listen(3000, () => {
+    console.log('listening on port 3000')
+})
